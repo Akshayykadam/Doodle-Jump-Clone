22d19c2cc1239db069fbb398a52600ccfb38b280
xKid <97009989+thexkid99@users.noreply.github.com>
2024-02-21 09:55:26 +0530

    minor changes

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
-
-    public TMPro.TMP_Text TopScore;
+        public TMPro.TMP_Text TopScore;
     // Start is called before the first frame update
     void Start()
     {
         TopScore.text = " Top Score : " + PlayerPrefs.GetFloat("TopScore");
-    }
-
-    
+    }    
 }
